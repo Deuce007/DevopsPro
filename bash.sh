8f3f55e3840f9e84d7ca7ea0059c60fe63503a6e
root <root@ip-172-31-33-5.eu-west-2.compute.internal>
2022-08-23 16:38:05 +0000

    to be transfered

--- bash.sh
+++ bash.sh
@@ -1,23 +1,18 @@
+#!/bin/bash
 
 sudo apt update
 
-sudo apt install docker-ce
+sudo apt-get install -y docker-ce
 
-sudo apt-get install jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt-get update
+
+sudo apt-get install -y jenkins
 
 sudo systemctl start jenkins
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
